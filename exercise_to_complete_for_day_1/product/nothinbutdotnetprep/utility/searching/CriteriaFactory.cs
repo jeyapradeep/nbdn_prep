6a0b80cbd31d5adeff47dfebf69494304c073585
unknown <Pradeep_Sakkaravarth@.aus.amer.dell.com>
2010-05-25 18:12:39 -0500

    Badly working Not

--- exercise_to_complete_for_day_1/product/nothinbutdotnetprep/utility/searching/CriteriaFactory.cs
+++ exercise_to_complete_for_day_1/product/nothinbutdotnetprep/utility/searching/CriteriaFactory.cs
@@ -5,27 +5,36 @@ namespace nothinbutdotnetprep.utility.searching
     public class CriteriaFactory<ItemToFilter, PropertyType>
     {
         public Func<ItemToFilter, PropertyType> accessor;
+        bool inverse;
 
         public CriteriaFactory(Func<ItemToFilter, PropertyType> accessor)
         {
             this.accessor = accessor;
         }
 
+        public CriteriaFactory<ItemToFilter, PropertyType> not
+        {
+            get
+            {
+                inverse = inverse ^ true;
+                return this;
+            }
+        }
+
         public Criteria<ItemToFilter> equal_to(PropertyType value)
         {
             return new PropertyCriteria<ItemToFilter, PropertyType>(accessor,
-                                                                    new EqualToCriteria<PropertyType>(value));
-        }
-
-        public Criteria<ItemToFilter> not_equal_to(PropertyType propertyValue)
-        {
-            return new NotCriteria<ItemToFilter>(equal_to(propertyValue));
+                                                                    inverse
+                                                                        ? (Criteria<PropertyType>) new NotCriteria<PropertyType>(new EqualToCriteria<PropertyType>(value))
+                                                                        : new EqualToCriteria<PropertyType>(value));
         }
 
         public Criteria<ItemToFilter> equal_to_any(params PropertyType[] property_values)
         {
             return new PropertyCriteria<ItemToFilter, PropertyType>(accessor,
-                                                                    new EqualToAnyCriteria<PropertyType>(property_values));
+                                                                   inverse
+                                                                       ? (Criteria<PropertyType>) new NotCriteria<PropertyType>(new EqualToAnyCriteria<PropertyType>(property_values))
+                                                                       : new EqualToAnyCriteria<PropertyType>(property_values));
         }
     }
 }
--- exercise_to_complete_for_day_1/product/nothinbutdotnetprep/utility/searching/Where.cs
+++ exercise_to_complete_for_day_1/product/nothinbutdotnetprep/utility/searching/Where.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace nothinbutdotnetprep.utility.searching
 {
 
